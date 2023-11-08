389f69696064d83c6d6169b8aa6865f2a1d72380
VaitoSoi <duyduy15456@gmail.com>
2023-11-08 21:07:42 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Olivia } from "./lib";
+import { config } from 'dotenv'
 
+config({ path: './.env' })
 const bot = new Olivia({ discord: { token: 'MTExODM4MTg0NDU2ODczNTc2NQ.GCRsWZ.IQr8LzCZsYLKhYNZpjzjlIHxQA2DNm_gbAA_WU' } })
 void bot.run()
 
