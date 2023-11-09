e77ed6c75c39a74ddc7678d8a63034f752424f8a
VaitoSoi <duyduy15456@gmail.com>
2023-11-09 22:16:50 +0700

    Update emptyQueue.ts

--- src/events/emptyQueue.ts
+++ src/events/emptyQueue.ts
@@ -5,4 +5,7 @@ export default new EventBuilder()
     .setName(GuildQueueEvent.emptyQueue)
     .setOnce(false)
     .setType('player')
-    .setExecute((olivia, queue: GuildQueue) => void queue.setRepeatMode(QueueRepeatMode.OFF) && queue.node.stop())
+    .setExecute((olivia, queue: GuildQueue) => {
+        queue.setRepeatMode(QueueRepeatMode.OFF);
+        queue.node.stop();
+    })
